acdd944ef819a8aaf623b04b762334c941b9074d
jerateeps <jerateep.s@gmail.com>
2021-06-30 00:11:04 +0700

    first commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using API.Data;
+using API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _ent;
+        public UsersController(DataContext ent)
+        {
+            _ent = ent;
+        }
+        [HttpGet]
+        public IEnumerable<AppUser> GetUsers() => _ent.AppUser.ToList();
+        [HttpGet("{_id}")]
+        public AppUser GetUsers(int _id) => _ent.AppUser.Find(_id);
+    }
+}
---
+++ API/Data/DataContext.cs
@@ -0,0 +1,35 @@
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        //public DataContext()
+        //{
+
+        //}
+        public DataContext(DbContextOptions<DataContext> options)
+          : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                //string Constr = "server=localhost;database=KMS;user=root;password=Abc@12345;TreatTinyAsBoolean=true;";
+                //optionsBuilder.UseSqlServer(Constr, o => o.EnableRetryOnFailure());
+            }
+        }
+        public DbSet<AppUser> AppUser { get; set; }
+ 
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //}
+    }
+}
